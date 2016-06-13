ba8642de3c0230583cdd14cb99cad661810cfb84
Marc <imapunkclaimer@gmail.com>
2016-06-13 16:24:38 -0700

    Add HotDog class

---
+++ RaysHotDogs.Core/HotDog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaysHotDogs.Core
+{
+    class HotDog
+    {
+        public int HotDogId { get; set; }
+        public int Price { get; set; }
+        public int PrepTime { get; set; }
+        public string ImagePath { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public bool Available { get; set; }
+        public bool Favorite { get; set; }
+        public List<string> Ingredients { get; set; }
+
+    }
+}
