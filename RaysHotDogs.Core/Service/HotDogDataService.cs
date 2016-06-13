3bd5286e13217bf22308fa3276147a21790db655
Marc <imapunkclaimer@gmail.com>
2016-06-13 16:36:19 -0700

    Add HotDogDataService class

---
+++ RaysHotDogs.Core/Service/HotDogDataService.cs
@@ -0,0 +1,20 @@
+﻿using RaysHotDogs.Core.Model;
+using RaysHotDogs.Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaysHotDogs.Core.Service
+{
+    class HotDogDataService
+    {
+        private static HotDogRepository hotDogRepository = new HotDogRepository();
+
+        public List<HotDogGroup> GetGroupedHotDogs()
+        {
+            return null;
+        }
+    }
+}
