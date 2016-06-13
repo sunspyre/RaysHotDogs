4a6f761291f3908a5943bf3b2dad29eaf40c4942
Marc <imapunkclaimer@gmail.com>
2016-06-13 16:32:13 -0700

    Add HotDogRepository test data

---
+++ RaysHotDogs.Core/Repository/HotDogRepository.cs
@@ -0,0 +1,36 @@
+﻿using RaysHotDogs.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaysHotDogs.Core.Repository
+{
+    public class HotDogRepository
+    {
+        private static List<HotDogGroup> hotDogGroups = new List<HotDogGroup>()
+        {
+            new HotDogGroup()
+            {
+                HotDogGroupId = 1, Title = "Meat Lovers", ImagePath = "", HotDogs = new List<HotDog>()
+                {
+                    new HotDog
+                    {
+                        Available = true,
+                        Description = "Meat lovers"
+                    }
+                }
+
+            }
+        };
+
+        public List<HotDog> GetAllHotDogs()
+        {
+            return null;
+        }
+
+    }
+
+
+}
