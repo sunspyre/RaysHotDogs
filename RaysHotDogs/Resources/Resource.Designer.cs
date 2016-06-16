3b0b91742de5c4cec2e5cf2d49070faf59b1cea4
Marc <Marc@MARC-PC>
2016-06-16 14:00:12 -0700

    Add basic template to HotDogDetailView

--- RaysHotDogs/Resources/Resource.Designer.cs
+++ RaysHotDogs/Resources/Resource.Designer.cs
@@ -60,14 +60,35 @@ namespace RaysHotDogs
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050002
-			public const int MyButton = 2131034114;
+			// aapt resource value: 0x7f050004
+			public const int DescriptionTextView = 2131034116;
 			
-			// aapt resource value: 0x7f050000
-			public const int hotDogImageView = 2131034112;
+			// aapt resource value: 0x7f050009
+			public const int MyButton = 2131034121;
+			
+			// aapt resource value: 0x7f050007
+			public const int amountEditText = 2131034119;
+			
+			// aapt resource value: 0x7f050006
+			public const int button1 = 2131034118;
 			
 			// aapt resource value: 0x7f050001
-			public const int hotDogNameTextView = 2131034113;
+			public const int hotDogImageView = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int hotDogNameTextView = 2131034114;
+			
+			// aapt resource value: 0x7f050000
+			public const int mainScrollView = 2131034112;
+			
+			// aapt resource value: 0x7f050008
+			public const int orderButton = 2131034120;
+			
+			// aapt resource value: 0x7f050005
+			public const int priceTextView = 2131034117;
+			
+			// aapt resource value: 0x7f050003
+			public const int shortDescriptionTextView = 2131034115;
 			
 			static Id()
 			{
