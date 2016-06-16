614fabf91dedfd28777f52ebbdffde7e54b8a305
Marc <Marc@MARC-PC>
2016-06-16 13:30:09 -0700

    Add HotDogDetail activity

--- RaysHotDogs/MainActivity.cs
+++ RaysHotDogs/MainActivity.cs
@@ -17,14 +17,9 @@ namespace RaysHotDogs
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-
-            // Get our button from the layout resource,
-            // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.MyButton);
-
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            button.Click += delegate { button.Text = $"{count++} clicks!"; };
         }
     }
 }
--- RaysHotDogs/Resources/Resource.Designer.cs
+++ RaysHotDogs/Resources/Resource.Designer.cs
@@ -60,8 +60,14 @@ namespace RaysHotDogs
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050002
+			public const int MyButton = 2131034114;
+			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int hotDogImageView = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int hotDogNameTextView = 2131034113;
 			
 			static Id()
 			{
@@ -77,7 +83,10 @@ namespace RaysHotDogs
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int HotDogDetailView = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
