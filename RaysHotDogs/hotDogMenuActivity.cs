80064fa985181f2dc4f58a38c84ee2e8ac8f9e13
Marc <Marc@MARC-PC>
2016-06-17 15:01:06 -0700

    Add Menu activity

---
+++ RaysHotDogs/hotDogMenuActivity.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using RaysHotDogs.Core;
+using RaysHotDogs.Core.Service;
+
+namespace RaysHotDogs
+{
+    [Activity(Label = "hotDogMenuActivity", MainLauncher = true)]
+    public class hotDogMenuActivity : Activity
+    {
+        private ListView hotDogListView;
+        private List<HotDog> allHotDogs;
+        private HotDogDataService hotDogDataService;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
