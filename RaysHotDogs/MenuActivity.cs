958c799d1305694191ff50fd3861a8ad4969acf5
Marc <Marc@MARC-HOME>
2016-06-18 20:03:46 -0700

    Add MainActivity event handlers

--- RaysHotDogs/MenuActivity.cs
+++ RaysHotDogs/MenuActivity.cs
@@ -15,11 +15,44 @@ namespace RaysHotDogs
     [Activity(Label = "MenuActivity", MainLauncher =true)]
     public class MenuActivity : Activity
     {
+        private Button orderButton;
+        private Button cartButton;
+        private Button aboutButton;
+        private Button mapButton;
+        private Button takePictureButton;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Create your application here
+            SetContentView(Resource.Layout.HotDogMenuView); //Sets the view to HotDogMenuView fore this activity
+
+            FindViews();
+            HandleEvents();
+        }
+
+        private void FindViews()
+        {
+            //Instantiate all buttons on page to access them from code
+            orderButton = FindViewById<Button>(Resource.Id.orderButton);
+            cartButton = FindViewById<Button>(Resource.Id.cartButton);
+            aboutButton = FindViewById<Button>(Resource.Id.aboutButton);
+            mapButton = FindViewById<Button>(Resource.Id.mapButton);
+            takePictureButton = FindViewById<Button>(Resource.Id.takePictureButton);
+        }
+
+        private void HandleEvents()
+        {
+            //Give the order button a click event
+            orderButton.Click += OrderButton_Click;
+        }
+
+        private void OrderButton_Click(object sender, EventArgs e)
+        {
+            //(Content packageContents, Type type)
+            var intent = new Intent(this, typeof(hotDogMenuActivity));
+
+            StartActivity(intent);
         }
     }
 }
